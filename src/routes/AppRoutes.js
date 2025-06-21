e13d04366033fff39f7d990fb5a23630663e4239
virajsparekh <viraj.parekh23@outlook.com>
2025-06-21 10:11:40 -0400

    Initial commit: React boilerplate via Vite

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        {/* Define routes here */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRoutes;
